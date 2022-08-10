0032ea072fa25a3836eda0cca139fdb0a1d81fce
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-10 19:08:13 -0300

    Update produtos.js

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,4 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/')
+router.get('/' , function(req , res , next){
+    res.status(200).send({
+        mensagem: "usando get dentro da rota de produtos"
+    })
+
+})
+
+router.post('/' , function(req , res , next){
+    res.status(201).send({
+        mensagem: "usando post dentro da rota de produtos"
+    })
+
+})
+
+module.exports = router
