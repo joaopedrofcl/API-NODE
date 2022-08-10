fec28b4b2023a5dd056d6a0d9068d3be7b571fd6
João Pedro Fernandes <joaopedrofclol@gmail.com>
2022-08-10 19:40:32 -0300

    Update produtos.js

--- routes/produtos.js
+++ routes/produtos.js
@@ -5,14 +5,30 @@ router.get('/' , function(req , res , next){
     res.status(200).send({
         mensagem: "usando get dentro da rota de produtos"
     })
-
 })
 
 router.post('/' , function(req , res , next){
     res.status(201).send({
         mensagem: "usando post dentro da rota de produtos"
     })
-
 })
+
+router.get('/:id_produto' , function(req , res , next){
+    const id = req.params.id_produto
+    if (id==="especial"){
+        res.status(200).send({
+        mensagem: "você encontrou o id especial" ,
+        id: id
+        })
+    }
+    else{
+        res.status(200).send({
+        mensagem: "você enviou um id" 
+    })
+}
+    
+})
+
+
 
 module.exports = router
